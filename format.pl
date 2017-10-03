c42b3682fa6f16de948d384f9cbd95582a2427b4
Erik Peldan <ep@cote.se>
2017-10-03 21:12:15 +0200

    Move formatting logic out of inspect.pl

---
+++ format.pl
@@ -0,0 +1,60 @@
+
+%
+% Module for Formatting and Pretty-Printing
+%
+
+:- module(format, []).
+
+:- use_module(inspect).
+
+print_constants(ClassContents) :-
+  constant_pool(ClassContents, Cs),
+  forall(nth1(Ix, Cs, C),
+         print_constant(Ix, C)).
+
+print_constant(Ix, C) :-
+  constant_type(C, Type),
+  constant_summary(Type, C, Summary),
+  format("~t~d~3+ ~w~t~30+~w~t~30+~n", [Ix, Type, Summary]).
+
+
+constant_summary(constant_utf8_info, C, Summary) :-
+  member(bytes(Codes), C),
+  format(string(Summary), "\"~s\"", [Codes]).
+
+constant_summary(Tag, C, Summary) :-
+  \+ Tag = constant_utf8_info,
+  format(string(LongSummary), "~w", [C]),
+  truncated_string(LongSummary, 70, Summary).
+
+truncated_string(LongString, Length, ShortString) :-
+  Length0 is Length - 3,
+  sub_string(LongString, 0, Length0, _After, ShortString0), 
+  !,
+  string_concat(ShortString0, "...", ShortString).
+
+truncated_string(ShortString, _Length, ShortString).
+
+pretty_constants(ClassContents, Constants) :-
+  constant_pool(ClassContents, Constants0),
+  maplist(pretty_constant, Constants0, Constants).
+
+pretty_constant(C0, C) :-
+  member(tag(Tag), C0),
+  spec_parse:cp_info_tag(Name, Tag),
+  standard_constant(Name),
+  functor(F, Name, 1),
+  arg(1, F, C0),
+  F = C.
+
+pretty_constant(C0, C) :-
+  inspect:constant_type(C0, constant_utf8_info),
+  member(bytes(B), C0),
+  string_codes(C, B).
+
+standard_constant(Name) :-
+  \+ Name = constant_utf8_info.
+
+constant_type(C, Type) :-
+  member(tag(T), C),
+  spec_parse:cp_info_tag(Type, T).
--- inspect.pl
+++ inspect.pl
@@ -7,10 +7,6 @@
 %
 % Top-Level API
 %
-print_constants(ClassContents) :-
-  constant_pool(ClassContents, Cs),
-  forall(nth1(Ix, Cs, C),
-         print_constant(Ix, C)).
 
 referenced_classes(ClassContents, ClassNames) :-
   constant_pool(ClassContents, Constants),
@@ -18,6 +14,9 @@ referenced_classes(ClassContents, ClassNames) :-
         constant_class_name(Constants, ClassName),
         ClassNames).
 
+
+constant_pool(ClassContents, ConstantPool) :-
+    member(constant_pool(ConstantPool), ClassContents).
 
 %
 % Helpers
@@ -30,56 +29,11 @@ constant_class_name(Constants, ClassName) :-
   nth1(Ix, Constants, Utf8),
   utf8_string(Utf8, ClassName).
 
-print_constant(Ix, C) :-
-  constant_type(C, Type),
-  constant_summary(Type, C, Summary),
-  format("~t~d~3+ ~w~t~30+~w~t~30+~n", [Ix, Type, Summary]).
-
 
 utf8_string(Utf8, String) :-
   member(bytes(Codes), Utf8),
   string_codes(String, Codes).
 
-constant_summary(constant_utf8_info, C, Summary) :-
-  member(bytes(Codes), C),
-  format(string(Summary), "\"~s\"", [Codes]).
-
-constant_summary(Tag, C, Summary) :-
-  \+ Tag = constant_utf8_info,
-  format(string(LongSummary), "~w", [C]),
-  truncated_string(LongSummary, 70, Summary).
-
-truncated_string(LongString, Length, ShortString) :-
-  Length0 is Length - 3,
-  sub_string(LongString, 0, Length0, _After, ShortString0), 
-  !,
-  string_concat(ShortString0, "...", ShortString).
-
-truncated_string(ShortString, _Length, ShortString).
-
-constant_pool(ClassContents, ConstantPool) :-
-    member(constant_pool(ConstantPool), ClassContents).
-
-pretty_constants(ClassContents, Constants) :-
-  constant_pool(ClassContents, Constants0),
-  maplist(pretty_constant, Constants0, Constants).
-
-pretty_constant(C0, C) :-
-  member(tag(Tag), C0),
-  spec_parse:cp_info_tag(Name, Tag),
-  standard_constant(Name),
-  functor(F, Name, 1),
-  arg(1, F, C0),
-  F = C.
-
-pretty_constant(C0, C) :-
-  member(tag(Tag), C0),
-  spec_parse:cp_info_tag(constant_utf8_info, Tag),
-  member(bytes(B), C0),
-  string_codes(C, B).
-
-standard_constant(Name) :-
-  \+ Name = constant_utf8_info.
 
 constant_type(C, Type) :-
   member(tag(T), C),
